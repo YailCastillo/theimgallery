ad200039cfb0c516641ac7cf8d8d3468723e5a21
YailCastillo <yailcastillomtz@gmail.com>
2024-01-17 17:31:08 -0600

    Ignorar cambios

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=theimgallery',
     'username' => 'root',
-    'password' => 'perolin1',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
